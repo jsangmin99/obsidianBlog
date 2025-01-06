45673bb5cf6b1f18218c6033eb9f8733fdd0737b
전상민 <95060314+jsangmin99@users.noreply.github.com>
2025-01-07 00:01:18 +0900

    Update quartz.config.ts

--- quartz.config.ts
+++ quartz.config.ts
@@ -8,7 +8,7 @@ import * as Plugin from "./quartz/plugins"
  */
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "🪴 Quartz 4.0",
+    pageTitle: "달도꺠비 두번쨰 뇌",
     pageTitleSuffix: "",
     enableSPA: true,
     enablePopovers: true,
